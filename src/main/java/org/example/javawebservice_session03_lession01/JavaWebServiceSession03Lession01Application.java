6831bb233dd147779635de22c1767139dc9bc709
pthongnhung <pthongnhung13122006@gmail.com>
2026-05-18 08:23:46 +0700

    add

---
+++ src/main/java/org/example/javawebservice_session03_lession01/JavaWebServiceSession03Lession01Application.java
@@ -0,0 +1,13 @@
+package org.example.javawebservice_session03_lession01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebServiceSession03Lession01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebServiceSession03Lession01Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javawebservice_session03_lession01/JavaWebServiceSession03Lession01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javawebservice_session03_lession01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebServiceSession03Lession01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
